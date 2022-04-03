3bb70e209e6e49a60632a899d08c3f8acea3aaac
AhmetKaraduman <karadumana@itu.edu.tr>
2022-04-03 16:18:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Hi,
+
+#
+
+# Text to speech
+
+A text to speech app that allows the user to communicate his/her basic needs by tapping on pictures or text.
+
+```bash
+for more detail,
+```
+
+[Brad Traversy - Repository](https://github.com/bradtraversy/vanillawebprojects)
---
+++ script.js
@@ -0,0 +1,39 @@
+const toggleBtn = document.getElementById("toggle-btn");
+const toggleContainer = document.getElementById("toggle-container");
+const closeBtn = document.querySelector(".x");
+const texts = document.querySelectorAll(".card-text");
+const readBtn = document.getElementById("read-btn");
+const textArea = document.getElementById("text-area");
+const allLang = document.querySelector("select");
+const cards = document.querySelectorAll(".card");
+var synth = window.speechSynthesis;
+
+// show toggle container
+toggleBtn.addEventListener("click", () => {
+	toggleContainer.classList.toggle("show");
+});
+
+// hide toggle container
+closeBtn.addEventListener("click", () => {
+	toggleContainer.classList.remove("show");
+});
+
+// click the card and speak text with selected lang
+cards.forEach((card) =>
+	card.addEventListener("click", () => {
+		sayIt(card.querySelector("h3").innerText);
+		card.classList.toggle("active");
+		setTimeout(() => card.classList.toggle("active"), 1000);
+	})
+);
+
+// read btn when typing something in text area and pick language
+readBtn.addEventListener("click", () => {
+	sayIt(textArea.value);
+});
+
+function sayIt(text) {
+	let utterance = new SpeechSynthesisUtterance(text);
+	utterance.lang = allLang.selectedOptions[0].getAttribute("data-lang");
+	speechSynthesis.speak(utterance);
+}
